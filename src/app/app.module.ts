224bf044153ab19a110c1a46682f1f60bc82280f
PabloSnhz <pablosanhuezaaa@gmail.com>
2023-11-13 15:01:09 -0300

    problema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CounterModule } from './counter/counter.module';
 import { HeroesModule } from './heroes/heroes.module';
-
+import { DbzModule } from './dbz/dbz.module';
 
 import { AppComponent } from './app.component';
-
 
 
 @NgModule({
@@ -17,6 +16,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     CounterModule,
     HeroesModule,
+    DbzModule
   ],
   providers: [],
   bootstrap: [AppComponent]
